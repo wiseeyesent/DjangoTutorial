f6eff23cf1e2238936ded6b79ef7d81e9881d15e
Joshua Cripe <admin@wiseeyesent.com>
2017-04-11 18:09:10 -0500

    Part V: The Fifth, Tests

--- mysite/settings.py
+++ mysite/settings.py
@@ -28,6 +28,8 @@ DEBUG = True
 ALLOWED_HOSTS = [
     'dev.wiseeyesent.com',
     'localhost',
+    '127.0.0.1',
+    'testserver',
 ]
 
 
--- polls/models.py
+++ polls/models.py
@@ -10,7 +10,8 @@ class Question(models.Model):
     def __str__(self):
         return self.question_text
     def was_published_recently(self):
-        return self.pub_date >= timezone.now() - datetime.timedelta(days=7)
+        now = timezone.now()
+        return now - datetime.timedelta(days=7) <= self.pub_date <= now
     question_text = models.CharField(max_length=200)
     pub_date = models.DateTimeField('date published')
 
